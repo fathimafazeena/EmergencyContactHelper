dc53099cc2f184f595e12a4d8a83b149cd3984fb
Fazeena <fathima.fazeena@gmail.com>
2026-03-12 13:53:56 +0530

    login screen re-added.

--- app/src/main/java/com/example/emergencycontacthelperr/LoginActivity.java
+++ app/src/main/java/com/example/emergencycontacthelperr/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         db = new DatabaseHelper(this);
 
